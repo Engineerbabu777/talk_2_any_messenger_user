d4f9f8b1a54eeac8945d39e2771714cb485456cc
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-12-05 19:34:56 +0500

    'session-1'

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const { errorHandler, notFound } = require('./middleware/errorMiddleware');
@@ -15,6 +16,8 @@ app.use(express.json());
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes)
+app.use("/api/message", messageRoutes);
+
 
 app.use(notFound);
 app.use(errorHandler);
